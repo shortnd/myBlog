ae7cb161fbce5fa9770f359150764da3b5be384a
shortnd <ococncol@gmail.com>
2019-05-10 07:49:35 -0400

    filesystem and remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/pages`,
+        name: 'myPage',
+      }
+    },
+    'gatsby-transformer-remark',
   ],
 }
